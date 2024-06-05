dcb4cc5ef64b274852468a83eae56651ad86f4ce
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-06-05 18:56:46 +0200

    Repository

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.student;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
