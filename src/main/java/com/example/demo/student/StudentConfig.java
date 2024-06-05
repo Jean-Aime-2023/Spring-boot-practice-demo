828eb65dbe5e3dd125c97eb464c3de614ea4929d
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-06-05 19:04:29 +0200

    StudentConfig

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student aime = new Student(
+                    "Aime",
+                    "aime@gmail.com",
+                    LocalDate.of(2000, 1, 5),
+                    21
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2001, 3, 12),
+                    21
+            );
+
+            repository.saveAll(
+                List.of(aime,alex)
+            );
+        };
+    }
+}
